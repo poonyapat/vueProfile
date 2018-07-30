cb162a3c71ce78fa09ffcc723c46be04aabe8697
Zyle <poonyapat.y@ku.th>
2018-07-30 19:06:36 +0700

    Add MenuOnTop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
+    <menu-on-top />
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuOnTop from './components/MenuOnTop.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MenuOnTop
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0px;
 }
 </style>
---
+++ src/components/MenuOnTop.vue
@@ -0,0 +1,68 @@
+<template>
+    <div ref="menu" :style="{position: position}">
+        <a v-for="menuItem in menuItems" :key="menuItem">
+            {{ menuItem }}
+        </a>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "MenuOnTop",
+        data() {
+            return {
+                menuItems: ["Home", "Poject", "Contact", "About"],
+                position: "relative"
+            }
+        },
+        methods: {
+            onScroll() {
+                if (document.documentElement.scrollTop > this.$refs.menu.clientHeight) {
+                    this.position = "fixed"
+                }
+                else {
+                    this.position = "relative"
+                }
+            },
+        },
+        created() {
+            window.addEventListener("scroll", this.onScroll)
+        },
+        destroyed() {
+            window.removeEventListener("scroll", this.onScroll)
+        },
+    }
+</script>
+
+<style scoped>
+div {
+    width: 100%;
+    background-color: black;
+    overflow: hidden;
+    position: relative;
+}
+a {
+    font-size: 25px;
+    color: #aaddee;
+    background-color: black;
+    text-decoration: none;
+    border: none;
+    display: block;
+    padding: 8px 16px;
+    float: left;
+}
+
+a:hover {
+    color: black;
+    background-color:#bbddff;
+}
+
+.relativePosition {
+    position: relative;
+}
+
+.fixedPosition {
+    position: fixed;
+}
+</style>
