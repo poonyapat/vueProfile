d7cc2832120e69456299ef5b4fcd188662b4f0f3
Zyle <poonyapat.y@ku.th>
2018-08-01 17:23:16 +0700

    move HomeLayout to home directory

---
+++ src/components/layout/home/HomeLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <box-template>
+        <multi-tab :button-tab="buttonTab" :current-tab="currentTab"></multi-tab>
+    </box-template>
+</template>
+
+<script>
+    import MultiTab from "../sub/MultiTab.vue"
+    import BoxTemplate from "../sub/BoxTemplate.vue"
+
+    export default {
+        name: "HomeLayout",
+        components: {
+            BoxTemplate,
+            MultiTab
+        },
+        data() {
+            return {
+                buttonTab: ["General", "Education", "Programming", "Activity"],
+                currentTab: "General"
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
