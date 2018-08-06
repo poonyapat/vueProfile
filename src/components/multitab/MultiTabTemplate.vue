8fc05d32366284dbb8a8f6a80eb1cbddf47a911b
Zyle <poonyapat.y@ku.th>
2018-08-06 14:38:29 +0700

    remove unused components

--- src/components/multitab/MultiTabTemplate.vue
+++ src/components/multitab/MultiTabTemplate.vue
@@ -1,7 +1,6 @@
 <template>
     <box-template>
         <multi-tab-menu :current-tab="currentTab" :tabs="tabs" v-on:change-tab="changeTab"></multi-tab-menu>
-        <!--<container-a-template :data="currentTab.content"></container-a-template>-->
         <keep-alive>
             <component :is="currentTab.content" style="padding: 1em;"></component>
         </keep-alive>
@@ -11,14 +10,12 @@
 <script>
     import MultiTabMenu from "./MultiTabMenu"
     import BoxTemplate from "../template/BoxTemplate"
-    import ContainerATemplate from "../template/ContainerATemplate"
 
     export default {
         name: "MultiTabTemplate",
         components:{
             BoxTemplate,
             MultiTabMenu,
-            ContainerATemplate
         },
         props: {
             tabs: {
